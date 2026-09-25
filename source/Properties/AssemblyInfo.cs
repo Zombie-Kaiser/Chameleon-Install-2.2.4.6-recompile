1270b904cbeda7175a9569c7d8be6ee4bd1bea0b
linux addict <141570642+Zombie-Kaiser@users.noreply.github.com>
2026-09-25 12:36:49 +0800

    Add files via upload

---
+++ source/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("www.wowpc.cn")]
+[assembly: AssemblyTrademark("wowpc")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct("Chameleon Install 2.2")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTitle("Chameleon Install 2.2")]
+[assembly: ComVisible(false)]
+[assembly: Guid("8e2133ce-d28d-4b29-85a2-b951b0ec7fb0")]
+[assembly: AssemblyFileVersion("2.2.4.6")]
+[assembly: AssemblyVersion("2.2.4.6")]
